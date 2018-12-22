0f27d021651746e1cedb81ceef4655de50449611
Damian Bojarowski <damian.bojarowski@interia.pl>
2018-12-22 12:58:34 +0100

    Prosta petla, if, i inne takie :D

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,15 +10,32 @@ namespace Hello
             //string imie1 = args[1];
             //Console.WriteLine("Witaj " + imie0 + " i ty " + imie1);
             //Console.WriteLine("Dzisiejsza data: " + DateTime.Now);
+
             int cyfra = 0;
             foreach (var imie in args)
             {
                 
                 cyfra = cyfra + 1;
-                Console.WriteLine("Witaj " + imie);
-                Console.WriteLine("Jesteś " + cyfra);
+                //Console.WriteLine("Witaj " + imie);
                 
             }
+
+            string osoby = "";
+            string ilosc = "";
+            if (cyfra == 1)
+            {
+                osoby = "Witaj " + args[0];
+                ilosc = "Jesteś sam";
+            }
+            if (cyfra == 2)
+            {
+                osoby = "Witaj " + args[0] + " i " + args[1];
+                ilosc = "Jest was dwóch";
+            }
+
+            Console.WriteLine("...");
+            Console.WriteLine(osoby);
+            Console.WriteLine(ilosc);
 
         }
     }
